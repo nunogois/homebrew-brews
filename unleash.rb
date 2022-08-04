3f11136d755170c6c4bd23b402bac5c80720b1e6
Nuno Góis <github@nunogois.com>
2022-08-04 23:55:04 +0100

    fix unleash-cli arm64 sha256

--- unleash.rb
+++ unleash.rb
@@ -4,17 +4,17 @@ class Unleash < Formula
   version "0.0.3"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.3/unleash-macos-arm64.tar.gz"
+    if Hardware::CPU.intel?
+      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.3/unleash-macos-x64.tar.gz"
       sha256 "5c6bc516274d26d6d42b156292fde621fe4d6e71f7174bc78e28e2d3b482f255"
 
       def install
         bin.install "unleash"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.3/unleash-macos-x64.tar.gz"
-      sha256 "5c6bc516274d26d6d42b156292fde621fe4d6e71f7174bc78e28e2d3b482f255"
+    if Hardware::CPU.arm?
+      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.3/unleash-macos-arm64.tar.gz"
+      sha256 "493fb83509f19861c6a2a5c86fc5a16d30357dac4b3f3b669222dc8c77e466fb"
 
       def install
         bin.install "unleash"
