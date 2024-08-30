cdf56623a862d42899c03243c17c7e574856e373
Nuno Góis <github@nunogois.com>
2024-08-30 16:28:32 +0100

    Update unleash.rb

--- unleash.rb
+++ unleash.rb
@@ -1,20 +1,20 @@
 class Unleash < Formula
   desc "CLI for interfacing with Unleash: https://www.getunleash.io/"
   homepage "https://github.com/nunogois/unleash-cli"
-  version "0.0.5"
+  version "0.0.6"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.5/unleash-macos-x64.tar.gz"
-      sha256 "79deee3447d53ca7c841cce8087c6204f65dbcd694937bd93b77e70a7e0294bd"
+      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.6/unleash-macos-x64.tar.gz"
+      sha256 "f401e52333975725e1473aa9fd7a99137beced09907debdd945b73a8d7fe9fb2"
 
       def install
         bin.install "unleash"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.5/unleash-macos-arm64.tar.gz"
-      sha256 "8cde0180ecd7ed39aed6a5fd2fb417ff7d647708bd4ddd80844226262bcc56c1"
+      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.6/unleash-macos-arm64.tar.gz"
+      sha256 "dee8cfb4f52d49387b62e3f00cc80311d7ed4f764768bfc4c1475504158fa3a1"
 
       def install
         bin.install "unleash"
@@ -24,16 +24,16 @@ class Unleash < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.5/unleash-linux-x64.tar.gz"
-      sha256 "0991c730084a3136e8e401e55a19e31cd054a04b219bda90e346e5e262e266e0"
+      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.6/unleash-linux-x64.tar.gz"
+      sha256 "14477e2bbbc46b03b38fe09086265eade712ea7e105f511ab95069fd23f00bf2"
 
       def install
         bin.install "unleash"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.5/unleash-linux-arm64.tar.gz"
-      sha256 "d8206b1c8bf4f603baff9f85dd8b601dbafc18a5b7f9e5bd2cd2cc5cf4e49991"
+      url "https://github.com/nunogois/unleash-cli/releases/download/v0.0.6/unleash-linux-arm64.tar.gz"
+      sha256 "3bd58e8141f42d8ee005d45a309b5dda7b245b8d3b91e049c186fdbf593eee38"
 
       def install
         bin.install "unleash"
